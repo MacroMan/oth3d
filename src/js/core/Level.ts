2096773fda90d589cc3f76c210467befbb4c1ec8
David Wakelin <david.wakelin@tutorful.co.uk>
2023-06-03 23:19:46 +0100

    Fix unable to remove floor tiles

--- src/js/core/Level.ts
+++ src/js/core/Level.ts
@@ -5,7 +5,7 @@ import Scene from "./Scene";
 export default class Level {
     readonly matrix: Matrix;
 
-    constructor (scene: Scene, data: LevelData) {
+    constructor(scene: Scene, data: LevelData) {
         this.matrix = new Matrix(scene, data.width, data.height, data.matrix);
         this.matrix.draw();
     }
--- src/js/core/Matrix/MatrixObject.ts
+++ src/js/core/Matrix/MatrixObject.ts
@@ -1,6 +1,6 @@
 import FloorTile from "./FloorTile";
 import WallTile from "./WallTile";
-import {Texture} from "./FloorTypes";
+import { Texture } from "./FloorTypes";
 import Scene from "../Scene";
 
 /**
@@ -13,28 +13,20 @@ export default class MatrixObject {
     public readonly wallTiles: WallTile[] = [];
     private readonly scene: Scene;
 
-    /**
-     * @param {Scene} scene
-     * @param {number} x
-     * @param {number} z
-     */
     constructor(scene: Scene, x: number, z: number) {
         this.scene = scene;
         this.x = x;
         this.z = z;
 
         // Add default floor tile of grass
-        this.floorTiles.push(new FloorTile(this.scene, {x: this.x, z: this.z, texture: Texture.grass}));
+        this.floorTiles.push(new FloorTile(this.scene, { x: this.x, z: this.z, texture: Texture.grass }));
     }
 
     /**
      * Add a new floor tile. Only the most recently added tile will be visible.
-     *
-     * @param texture
-     * @return FloorTile
      */
     addFloorTile(texture: Texture): FloorTile {
-        const floorTile = new FloorTile(this.scene, {x: this.x, z: this.z, texture: texture})
+        const floorTile = new FloorTile(this.scene, { x: this.x, z: this.z, texture: texture })
         this.floorTiles.push(floorTile);
         return floorTile;
     }
@@ -45,6 +37,8 @@ export default class MatrixObject {
     popFloorTile(): void {
         if (this.floorTiles.length > 1) {
             this.floorTiles.pop();
+
+            this.drawFloor();
         }
     }
 
